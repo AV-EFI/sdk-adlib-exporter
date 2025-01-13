159b9b726d76a5e4d3712cca61d12e375f570b6d
fjlein <fjlein@gmail.com>
2025-01-13 19:54:31 +0100

    now using dedicated caching package

--- adlib/__init__.py
+++ adlib/__init__.py
@@ -1,24 +1,23 @@
 import os
 from adlib.provider import RecordProvider, PointerFileProvider
 from adlib.wrapper import Database
+from requests_cache import CachedSession
 
-# Attempt to retrieve the Adlib URL from environment variables
+
+class CachedDatabase(Database):
+    def __init__(self, url):
+        super().__init__(url)
+        self.session = CachedSession(cache_name="adlib_cache")
+
+
 try:
     adlib_url = os.environ["SDK_ADLIB_URL"]
-    adlib = Database(adlib_url)
+    adlib = CachedDatabase(adlib_url)
 except KeyError:
     raise EnvironmentError("SDK_ADLIB_URL environment variable is not set.")
 
 # Initialize the different providers for Adlib data retrieval
-pointer_file_provider = PointerFileProvider(
-    adlib, cache=True, _dir="pointer", database="collect.inf"
-)
-collect_provider = RecordProvider(
-    adlib, cache=True, _dir="collect", database="collect.inf"
-)
-people_provider = RecordProvider(
-    adlib, cache=True, _dir="people", database="people.inf"
-)
-thesau_provider = RecordProvider(
-    adlib, cache=True, _dir="thesau", database="thesau.inf"
-)
+pointer_file_provider = PointerFileProvider(adlib, database="collect.inf")
+collect_provider = RecordProvider(adlib, database="collect.inf")
+people_provider = RecordProvider(adlib, database="people.inf")
+thesau_provider = RecordProvider(adlib, database="thesau.inf")
