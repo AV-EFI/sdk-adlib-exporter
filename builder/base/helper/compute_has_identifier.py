696e998258b599d619c8f4bef138ca45d0ef18bc
fjlein <fjlein@gmail.com>
2025-04-23 20:44:09 +0200

    Added avefi resource identifier

--- builder/base/helper/compute_has_identifier.py
+++ builder/base/helper/compute_has_identifier.py
@@ -2,5 +2,13 @@ from avefi_schema import model as efi
 
 
 def compute_has_identifier(self):
+    identifiers = []
+
     priref = self.xml.xpath("priref/text()")[0]
-    return efi.LocalResource(id=priref)
+    identifiers.append(efi.LocalResource(id=priref))
+
+    pid_list = self.xml.xpath("PIDdata/PID_data_URI/text()")
+    if pid_list is not None and len(pid_list) == 1:
+        identifiers.append(efi.AVefiResource(id=pid_list[0]))
+
+    return identifiers
