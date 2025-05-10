c8c60c2e7a4d82b2fe593685b0c542cb2bd181ce
fjlein <fjlein@gmail.com>
2025-05-10 15:00:29 +0200

    refactor

--- builder/work/helper/compute_has_event.py
+++ builder/work/helper/compute_has_event.py
@@ -12,8 +12,6 @@ from mappings.writing_activity_type_enum import writing_activity_type_enum
 
 
 def compute_has_event(self):
-    # Currently only production event
-
     # activity handling
 
     activities = []
@@ -37,26 +35,12 @@ def compute_has_event(self):
             name = name_list[0]
             priref = priref_list[0]
 
-            person_xml = people_provider.get_by_priref(priref)
-            sources_xml = person_xml.xpath("Source")
-
-            same_as = []
-
-            for source_xml in sources_xml:
-                # Note how is it called source.number in people.inf instead of term.number like in thesau.inf!
-                source_number_list = source_xml.xpath("source.number/text()")
-                if not source_number_list:
-                    # sometimes a source is provided but the source_number field is just empty e.g. 8072.xml
-                    continue
-                source_number = source_number_list[0]
-                if "gnd" in source_number:
-                    same_as.append(
-                        efi.GNDResource(
-                            id=source_number.split("/")[-1],
-                        )
-                    )
             cast_members.append(
-                efi.Agent(type=efi.AgentTypeEnum.Person, has_name=name, same_as=same_as)
+                efi.Agent(
+                    type=efi.AgentTypeEnum.Person,
+                    has_name=name,
+                    same_as=_get_same_as_for_priref(priref, people_provider),
+                )
             )
         except Exception as e:
             raise Exception("Problem with has_event (Cast):", e)
@@ -92,36 +76,25 @@ def compute_has_event(self):
                 name = name_list[0]
                 priref = priref_list[0]
 
-                person_xml = people_provider.get_by_priref(priref)
-                sources_xml = person_xml.xpath("Source")
-
-                same_as = []
-
-                for source_xml in sources_xml:
-                    source_number_list = source_xml.xpath("source.number/text()")
-                    if not source_number_list:
-                        continue
-                    source_number = source_number_list[0]
-                    if "gnd" in source_number:
-                        same_as.append(
-                            efi.GNDResource(
-                                id=source_number.split("/")[-1],
-                            )
-                        )
-
                 activities.append(
                     activity(
                         type=activity_type_enum[activity_type_name],
                         has_agent=efi.Agent(
                             type=efi.AgentTypeEnum.Person,
                             has_name=name,
-                            same_as=same_as,
+                            same_as=_get_same_as_for_priref(priref, people_provider),
                         ),
                     )
                 )
 
-    # date handling
+    return efi.ProductionEvent(
+        located_in=_get_located_in(self),
+        has_date=_get_has_date(self),
+        has_activity=activities,
+    )
 
+
+def _get_has_date(self):
     production_date_start = self.xml.xpath("Dating/dating.date.start/text()")
     production_date_start_prec = self.xml.xpath(
         "Dating/dating.date.start.prec/value[@lang='3'][text()='circa']/text()"
@@ -131,24 +104,21 @@ def compute_has_event(self):
         "Dating/dating.date.end.prec/value[@lang='3'][text()='circa']/text()"
     )
 
-    has_date = None
-
     if production_date_start and production_date_end:
-        has_date = get_formatted_date(
+        return get_formatted_date(
             production_date_start[0],
             production_date_start_prec[0] if production_date_start_prec else None,
             production_date_end[0],
             production_date_end_prec[0] if production_date_end_prec else None,
         )
 
-    # location handling
 
+def _get_located_in(self):
     located_in = []
 
     xml_productions = self.xml.xpath("Production")
 
     try:
-
         for xml_production in xml_productions:
             production_country_list = xml_production.xpath(
                 "production_country/value[@lang='de-DE']/text()"
@@ -157,35 +127,48 @@ def compute_has_event(self):
 
             if not production_country_list or not priref_list:
                 continue
+
             production_country_name = production_country_list[0]
             priref = priref_list[0]
 
-            production_country_xml = thesau_provider.get_by_priref(priref)
-            sources_xml = production_country_xml.xpath("Source")
-
-            same_as = []
-
-            for source_xml in sources_xml:
-                # Note how is it called source.number instead of term.number like in content_genre!
-                source_list = source_xml.xpath("source/text()")
-                source_number_list = source_xml.xpath("term.number/text()")
-                if not source_list or not source_number_list:
-                    continue
-                source = source_list[0]
-                source_number = source_number_list[0]
-                if source == "DNB":
-                    same_as.append(
-                        efi.GNDResource(
-                            id=source_number.split("/")[-1],
-                        )
-                    )
-
             located_in.append(
-                efi.GeographicName(has_name=production_country_name, same_as=same_as)
+                efi.GeographicName(
+                    has_name=production_country_name,
+                    same_as=_get_same_as_for_priref(priref, thesau_provider),
+                )
             )
     except Exception as e:
         raise Exception("Problem with has_event (location):", e)
 
-    return efi.ProductionEvent(
-        located_in=located_in, has_date=has_date, has_activity=activities
-    )
+    return located_in
+
+
+def _get_same_as_for_priref(priref, provider):
+    same_as = []
+
+    try:
+        xml_data = provider.get_by_priref(priref)
+        xml_sources = xml_data.xpath("Source")
+
+        for source_xml in xml_sources:
+            source_number_list = source_xml.xpath("source.number/text()")
+            if not source_number_list:
+                continue
+            source_number = source_number_list[0]
+            if "d-nb.info/gnd/" in source_number:
+                same_as.append(
+                    efi.GNDResource(
+                        id=source_number.split("/")[-1],
+                    )
+                )
+            if "www.filmportal.de" in source_number:
+                same_as.append(
+                    efi.FilmportalResource(
+                        id=source_number.split("_")[-1],
+                    )
+                )
+
+    except Exception as e:
+        raise Exception("Problem with same_as calculation:", e)
+
+    return same_as
