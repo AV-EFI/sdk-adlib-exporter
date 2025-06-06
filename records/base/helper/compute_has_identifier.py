3169513eecdc69e7eb50fd918d234b7ff7c6aeff
fjlein <fjlein@gmail.com>
2025-06-06 12:39:34 +0200

    pid path extraction update

--- records/base/helper/compute_has_identifier.py
+++ records/base/helper/compute_has_identifier.py
@@ -6,8 +6,11 @@ from records.base.base_record import BaseRecord
 def compute_has_identifier(record: BaseRecord):
     identifiers = [efi.LocalResource(id=record.priref)]
 
-    pid = record.xml.get_first("PIDdata/PID_data_URI/text()")
-    if pid is not None:
+    pid_url = record.xml.get_first("PIDdata/PID_data_URI/text()")
+
+    if pid_url is not None:
+        pid = pid_url.split("https://hdl.handle.net/")[-1]
+
         identifiers.append(efi.AVefiResource(id=pid))
 
     return identifiers
